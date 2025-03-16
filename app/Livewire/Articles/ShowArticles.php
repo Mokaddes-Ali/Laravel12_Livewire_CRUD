260177e21efb9e838039dad6cdf05c8e6c25b395
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2025-03-16 15:17:41 +0600

    first commit

---
+++ app/Livewire/Articles/ShowArticles.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Articles;
+
+use Livewire\Component;
+
+class ShowArticles extends Component
+{
+    public function render()
+    {
+        return view('livewire.articles.show-articles');
+    }
+}
