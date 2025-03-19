ad865e5b76aa00468d54d42d60c0d71789d58afc
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2025-03-19 13:47:16 +0600

    migrate message and model commit

--- app/Livewire/ChatBox.php
+++ app/Livewire/ChatBox.php
@@ -2,13 +2,19 @@
 
 namespace App\Livewire;
 
-use Livewire\Component;
+use App\Models\Message;
 use App\Models\User;
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class ChatBox extends Component
 {
     public $selectedUser;
+    public $message;
+    public $senderId;
+    public $receiverId;
+
 
     public function mount()
     {
@@ -16,6 +22,9 @@ class ChatBox extends Component
         if ($userId) {
             $this->selectedUser = User::find($userId);
         }
+        $this->senderId = auth()->id();
+        $this->receiverId = $this->selectedUser->id;
+
     }
 
     public function closeChat()
@@ -27,6 +36,39 @@ class ChatBox extends Component
     public function render()
     {
         return view('livewire.chat-box', ['selectedUser' => $this->selectedUser]);
+    }
+
+    public function sendMessage()
+    {
+       $this->saveMessage();
+
+       $this->message = null;
+    }
+
+    public function saveMessage()
+    {
+        // dd([
+        //     'sender_id' => $this->senderId,
+        //     'receiver_id'=> $this->receiverId,
+        //     'message' => $this->message,
+        //     // 'file_name',
+        //     // 'file_original_name',
+        //     // 'folder_path',
+        //     'is_read' => false,
+
+        // ]);
+
+
+        return Message::create([
+            'sender_id' => $this->senderId,
+            'receiver_id'=> $this->receiverId,
+            'message' => $this->message,
+            // 'file_name',
+            // 'file_original_name',
+            // 'folder_path',
+            'is_read' => false,
+
+        ]);
     }
 }
 
